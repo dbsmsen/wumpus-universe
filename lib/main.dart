a131c804e5e134fec5ccd1560a4222f640cbff34
Debsoomonto Sen <sendebsoomonto7149@gmail.com>
2025-05-06 19:21:50 +0530

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ import 'screens/onboarding_screen.dart';
 import 'screens/rules_screen.dart';
 import 'screens/initial_onboarding_screen.dart';
 import 'screens/login_screen.dart';
+import 'screens/grid_selection_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart'; // import the generated file
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +16,7 @@ void main() async {
   );
   runApp(const MyApp());
 }
+
 void addSampleData() async {
   try {
     await FirebaseFirestore.instance.collection('users').add({
@@ -43,7 +44,7 @@ class MyApp extends StatelessWidget {
       // Start at login screen
       home: const LoginScreen(),
       routes: {
-        '/game': (context) => const GameScreen(),
+        '/grid_selection': (context) => const GridSelectionScreen(),
         '/welcome': (context) => const InitialOnboardingScreen(),
         '/tutorial': (context) => const OnboardingScreen(),
         '/rules': (context) => const RulesScreen(),
